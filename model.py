bbf59ba147c550eee3e33709fd6defa013625422
Zhicheng's Ubuntu <beyond6987@gmail.com>
2019-07-10 09:40:08 -0500

    modifiable->time_varying, unmodifiable->static

--- model.py
+++ model.py
@@ -6,18 +6,18 @@ import torch.nn.functional as F
 
 
 class FGAM(nn.Module):
-    def __init__(self, n_classes, dim_modifiable, dim_unmodifiable, n_embedding, n_hid, batch_norm=False):
+    def __init__(self, n_classes, dim_time_varying, dim_static, n_embedding, n_hid, batch_norm=False):
         super(FGAM, self).__init__()
 
         self.n_classes = n_classes
-        self.dim_modifiable = dim_modifiable
-        self.dim_unmodifiable = dim_unmodifiable
+        self.dim_time_varying = dim_time_varying
+        self.dim_static = dim_static
         self.n_embedding = n_embedding
         self.n_hid = n_hid
         self.batch_norm = batch_norm
         self.embedding_layers = nn.ModuleList()
 
-        for i in range(self.dim_modifiable):
+        for i in range(self.dim_time_varying):
             branch = nn.ModuleList()
             branch.append(nn.Linear(1, self.n_embedding))
             for _ in range(n_hid):
@@ -27,28 +27,28 @@ class FGAM(nn.Module):
             branch.append(nn.Linear(self.n_embedding, 128))
             branch.append(nn.Linear(128, 1))
             self.embedding_layers.append(branch)
-        self.linear = nn.Linear(self.dim_modifiable, self.n_classes)
+        self.linear = nn.Linear(self.dim_time_varying, self.n_classes)
 
         # for logits[0]
         self.weights_module = nn.ModuleList()
-        self.weights_module.append(nn.Linear(self.dim_unmodifiable, 128))
+        self.weights_module.append(nn.Linear(self.dim_static, 128))
         self.weights_module.append(nn.Linear(128, 128))
-        self.weight = nn.Linear(128, self.dim_modifiable*1)
+        self.weight = nn.Linear(128, self.dim_time_varying*1)
         self.bias = nn.Linear(128, 1)
 
         # for logits[1]
         self.weights_module2 = nn.ModuleList()
-        self.weights_module2.append(nn.Linear(self.dim_unmodifiable, 128))
+        self.weights_module2.append(nn.Linear(self.dim_static, 128))
         self.weights_module2.append(nn.Linear(128, 128))
-        self.weight2 = nn.Linear(128, self.dim_modifiable*1)
+        self.weight2 = nn.Linear(128, self.dim_time_varying*1)
         self.bias2 = nn.Linear(128, 1)
 
 
 
-    def forward(self, unchangeables, changeables):
+    def forward(self, static, time_varying):
         res = []
-        for i in range(self.dim_modifiable):
-            x = changeables[i]
+        for i in range(self.dim_time_varying):
+            x = time_varying[i]
             for j, op in enumerate(self.embedding_layers[i]):
                 # no relu before batchNorm
                 if (j < (len(self.embedding_layers[i])-1) and
@@ -60,8 +60,8 @@ class FGAM(nn.Module):
                     x = F.relu(op(x))
             res.append(x)
         x = torch.cat(res, 1)
-        w = unchangeables
-        w2 = unchangeables
+        w = static
+        w2 = static
         for i, op in enumerate(self.weights_module):
             w = F.relu(op(w))
         weight = self.weight(w)
